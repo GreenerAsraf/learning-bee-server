f416ac7d3557901e3ba50267d38f70f8fead7e95
Asraf407 <Asraf.uddin4407@gmail.com>
2022-10-25 08:40:34 +0600

    added

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('gitignore file deleted and git file updated')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port in the port of  ${port}`)
 })
